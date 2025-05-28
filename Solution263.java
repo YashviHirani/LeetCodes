9dd1d26cadfb65cf949c7c3be1e78c930f7c5065
Yashvi Hirani <yashvihirani0408@gmail.com>
2025-05-28 12:26:44 +0530

    leet-70

---
+++ Solution263.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Solution263 {
+    boolean ans = true;
+    public boolean isUgly(int n) {
+
+        ans = true;
+        if(n==1){
+            ans = true;
+            return ans;
+        }
+        if(n<=0){
+            ans = false;
+            return ans;
+        }
+        int i = 1;
+        while(n%2==0){
+
+            n /=2;
+        }
+        while(n%3==0){
+
+            n /=3;
+        }
+        while(n%5==0){
+
+            n /=5;
+        }
+
+        return n==1;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Solution263 s = new Solution263();
+
+        System.out.print("enter n - ");
+        int n = sc.nextInt();
+
+        System.out.println(s.isUgly(n));
+    }
+}
