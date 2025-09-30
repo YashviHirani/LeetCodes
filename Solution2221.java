8fede81a2792cb592ced493976f5c90d616b45f9
Yashvi Hirani <yashvihirani0408@gmail.com>
2025-09-30 16:58:51 +0530

    leet- 2221

---
+++ Solution2221.java
@@ -0,0 +1,15 @@
+public class Solution2221 {
+    public int triangularSum(int[] nums) {
+
+        if(nums.length==1) return nums[0]; //bc
+
+        int size = nums.length;
+        while (size>1) {
+            for (int i = 0; i < size - 1; i++) {
+                nums[i] = (nums[i] + nums[i + 1])%10;
+            }
+            size--;
+        }
+        return nums[0];
+    }
+}
