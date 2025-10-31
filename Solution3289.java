f5f54b56559b33953ba68bdf66f2658d974b7896
Yashvi Hirani <yashvihirani0408@gmail.com>
2025-10-31 16:39:45 +0530

    leet - 3289

---
+++ Solution3289.java
@@ -0,0 +1,17 @@
+public class Solution3289 {
+    public int[] getSneakyNumbers(int[] nums) {
+
+        int [] freq = new int[100];
+        int [] ans = new int[2];
+
+        int j = 0;
+        for (int num : nums) {
+            if (freq[num] == 1){
+                ans[j++] = num;
+                if(j==2) break;
+            }
+            else freq[num]++;
+        }
+        return ans;
+    }
+}
