da4edba0654f173a067fece7eeec9405cee1f64a
Yashvi Hirani <yashvihirani0408@gmail.com>
2025-11-10 12:00:53 +0530

    leet - 135

---
+++ Solution135.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        candy = [1]*len(ratings)
+        n = len(ratings)
+
+        for i in range(1,n):
+            if ratings[i]>ratings[i-1]:
+                candy[i] = candy[i-1]+1
+
+        for i in range(n-2,-1,-1):
+            if ratings[i]>ratings[i+1]:
+                candy[i] = max(candy[i],candy[i+1]+1)
+        return (sum(candy))
