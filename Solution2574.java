5ed3ffa51d531adf8fe85a060b449f5f9ed0d1eb
Yashvi Hirani <yashvihirani0408@gmail.com>
2025-11-18 22:23:10 +0530

    leet - 2574

---
+++ Solution2574.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Solution2574 {
+    public static int[] leftRightDifference(int[] nums) {
+
+        int[] leftsum = new int[nums.length];
+        int sum = 0;
+        int temp = 0;
+        for(int i = 0; i< nums.length; i++){
+            sum += nums[i];
+            leftsum[i] = temp;
+            temp += nums[i];
+        }
+         for(int i = 0; i< nums.length; i++){
+                nums[i] = Math.abs(leftsum[i] - (sum - leftsum[i]-nums[i]));
+         }
+        return nums;
+    }
+    public static void main(String[] args) {
+        System.out.println(leftRightDifference(new int[]{10,4,8,3}));
+    }
+}
