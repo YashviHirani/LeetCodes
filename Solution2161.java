bd9f22e2d19c3369efe012549f528bce59f628a1
Yashvi Hirani <yashvihirani0408@gmail.com>
2025-11-21 21:52:16 +0530

    leet - 2161

---
+++ Solution2161.java
@@ -0,0 +1,28 @@
+public class Solution2161 {
+    public static int[] pivotArray(int[] nums, int pivot) {
+
+        int[] ans = new int[nums.length];
+
+        // for elements  less than pivot
+        int j = 0;
+        int count = 0;
+        for(int i : nums){
+            if(i<pivot) ans[j++] = i;
+            else if (i==pivot) count++;
+        }
+        // for adding pivot occurence more than 1
+        while (count>0){
+            ans[j++] = pivot;
+            count--;
+        }
+        // for elements more than pivot
+        for(int i : nums){
+            if(i>pivot) ans[j++] = i;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pivotArray(new int[]{9,12,5,10,14,3,10},10));
+    }
+}
