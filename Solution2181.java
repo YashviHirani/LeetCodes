4d6ca4b215df07d98c7a95cd8c06d4454a8f7067
Yashvi Hirani <yashvihirani0408@gmail.com>
2025-12-12 22:14:48 +0530

    leet - 2181

---
+++ Solution2181.java
@@ -0,0 +1,28 @@
+public class Solution2181 {
+    public ListNode mergeNodes(ListNode head) {
+
+        if(head==null) return head;
+        ListNode ans = null;
+        ListNode ans1 = ans;
+        ListNode temp = head.next;
+        int sum = 0;
+        while(temp!=null){
+            if(temp.val!=0){
+                sum += temp.val;
+            }
+            else{
+                if(ans==null){
+                    ans = new ListNode(sum);
+                    ans1 = ans;
+                }
+                else{
+                    ans.next = new ListNode(sum);
+                    ans = ans.next;
+                }
+                sum = 0;
+            }
+            temp = temp.next;
+        }
+        return ans1;
+    }
+}
