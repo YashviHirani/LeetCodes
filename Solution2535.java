60f75d48d17a5866c29c7602a5cc894aec28eab5
Yashvi Hirani <yashvihirani0408@gmail.com>
2026-01-01 19:26:09 +0530

    leet - 2535

---
+++ Solution2535.java
@@ -0,0 +1,15 @@
+public class Solution2535 {
+    public int differenceOfSum(int[] nums) {
+        int sum = 0;
+        int Dsum = 0;
+
+        for(int i:nums){
+            sum += i;
+            while (i > 0){
+                Dsum += i%10;
+                i /= 10;
+            }
+        }
+        return Math.abs(Dsum-sum);
+    }
+}
