39aaee971e0b7e2621c58ec7465d0f41f083fae3
Yashvi Hirani <yashvihirani0408@gmail.com>
2026-01-27 20:33:00 +0530

    leet - 3340

---
+++ Solution3340.java
@@ -0,0 +1,21 @@
+class Solution3340 {
+    public static boolean isBalanced(String num) {
+        int sum1 = 0;
+        int sum2 = 0;
+        int i = 0;
+        int j = 1;
+        while (i < num.length()){
+            sum1 += (num.charAt(i)-'0');
+            i+=2;
+            if(j<num.length()){
+                sum2 += (num.charAt(j)-'0');
+                j+=2;
+            }
+        }
+        return sum2 == sum1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( isBalanced("24123"));
+    }
+}
